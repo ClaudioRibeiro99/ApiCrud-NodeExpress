4464dd661fe721123e17d3d6149d59460d6174e8
Cláudio Marcelo Ribeiro <claudio.ribeiro@lucree.com.br>
2022-05-06 09:53:56 -0300

    Instalação do dotenv e escondendo as variaveis

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
 
 //forma de ler json /middleware
 app.use(
@@ -16,14 +17,12 @@ const personRoutes = require('./routes/personRoutes')
 
 app.use('/person', personRoutes)
 
-
-// entregar uma porta 
-const DB_USER = 'claudio'
-const DB_PASSWORD = encodeURIComponent('FdyRht49WlNfhZf4')
+const dbUser = process.env.DB_USER
+const dbPassword = 'FdyRht49WlNfhZf4'//process.env.DB_PASSWORD
 
 mongoose
     .connect(
-        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.b1qcq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+        `mongodb+srv://${dbUser}:${dbPassword}@apicluster.b1qcq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     )
 .then(() =>{
     console.log('Conectamos ao MongoDB!')
